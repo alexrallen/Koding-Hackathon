7a7a13ea41bcbffadde75eaaf06f95a6a3152e75
robocoder99 <admin@altechmc.com>
2014-12-06 15:18:54 -0500

    Communicate!

--- legal.php
+++ legal.php
@@ -1,11 +1,3 @@
-<html>
-<body>
-
-legal <?php echo $_POST["legal"]; ?><br> 
-
-<?php echo "<textarea id="output" editable="false" readonly="true">
-This is what the kids call a Test 
-</textarea>"; ?> 
-
-</body>
-</html>
+<?php 
+	echo "It works!"; 
+?> 
